678b9f181bf6aa496884f84a68ed1c1c92273fc2
FarahBahajjaj <113434104+FarahBahajjaj@users.noreply.github.com>
2022-09-14 16:47:15 +0300

    Add files via upload

---
+++ Scripts/main.js
@@ -0,0 +1,36 @@
+const myImage = document.querySelector('img');
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/solar.jpg') {
+    myImage.setAttribute('src','images/solar2.jpeg');
+  } else {
+    myImage.setAttribute('src','images/solar.jpg');
+  }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    const myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `solar system, ${myName}`;
+  }
+  if (!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem('name');
+    myHeading.textContent = `solar system, ${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  }
+  function setUserName() {
+    const myName = prompt('Please enter your name.');
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = `solar system, ${myName}`;
+    }
+  }
+  
